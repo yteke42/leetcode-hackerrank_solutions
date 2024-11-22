4526ac3cbb97d05bd709584d2cd9030d7adee3e7
Yunusteke <yunusteke4646@gmail.com>
2024-11-23 02:36:50 +0300

    leetcode

---
+++ Leetcode/LongestCommonPrefix/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs.sort(key=len)
+        s=""
+        for i in range(len(strs[0])):
+            for j in range(1,len(strs)):
+                if strs[j][i]!=strs[0][i]: return s
+            s+=strs[0][i]
+        return s
---
+++ Leetcode/RomanToInteger/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        myDict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        total = 0
+        prev_value = 0
+
+        for i in reversed(s):
+            current_value = myDict[i]
+
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+
+            prev_value = current_value
+
+        return total
+
+# O(n) time complexity
